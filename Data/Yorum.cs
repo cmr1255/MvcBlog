429bbe85432641fc44fa5a6f33b05c3c48133317
cmr1255 <karacacemre1255@gmail.com>
2015-06-05 01:21:15 +0300

    mvc blog projesi

---
+++ Data/Yorum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcProjesi.Data
+{
+    public class Yorum
+    {
+        public int YorumId { get; set; }
+
+        [Required(ErrorMessage = "Lütfen yorumunuzu giriniz.")]
+        public string Icerik { get; set; }
+
+        [Required(ErrorMessage = "Lütfen yorumun yapılma tarihini giriniz.")]
+        [DataType(DataType.DateTime, ErrorMessage = "Lütfen yorum yapılma tarihini, doğru bir şekilde giriniz.")]
+        public DateTime Tarih { get; set; }
+
+        
+        public virtual Makale Makale { get; set; }
+
+        
+        public virtual Uye Uye { get; set; }
+    }
+}
